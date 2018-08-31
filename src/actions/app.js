a80fd2a65c55e45ff400360a267fb0b333ae0e01
Herat Patel <heratpatel1@gmail.com>
2018-08-31 23:54:46 +0400

    initial commit

--- src/actions/app.js
+++ src/actions/app.js
@@ -6,7 +6,7 @@ export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
 
 export const navigate = (path) => (dispatch) => {
   // Extract the page name from path.
-  const page = path === '/' ? 'welcome' : path.slice(1);
+  const page = path === '/' ? '/welcome' : path.slice(1);
 
   // Any other info you might want to extract from the path (like page type),
   // you can do here
